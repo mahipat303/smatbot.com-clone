20ae504d683c9c703ee3b61d8d5cd7d26ccbc742
imgyan <inex.gyan@gmail.com>
2022-08-24 15:34:43 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,77 @@
+let navbars=()=>{
+    return `  <div id="inner_navbar">
+    <div id="left_navbar">
+        <div id="left_up"> 
+            <div> <a href="index.html"><img src="https://www.smatbot.com/img/Header/Logo_sp.svg" alt=""></a></div>
+          <div>  <a href="index.html"><h2>SmatBot</h2></a></div> 
+        </div>
+      <div><p>Subsidiary of ByteQuark</p></div> 
+     </div>
+
+<div id="right_navbar">
+    <div>
+        <div class="class-menu">
+            <button class="class-button">Product</button>
+            <div class="dropdownmenu">
+                <a href="chatbot.html">Chatbot</a>
+                <a href="liveChat.html">Live Chat</a>
+                <a href="useCases.html">UseCases</a>
+                <a href="marketing&Sales.html">Marketing & Sales</a>
+                <a href="support.html">Support</a>
+                <a href="templates.html">Templates</a>
+            </div>
+        </div>
+        <div class="class-menu">
+            
+                <button class="class-button"><a href="WhatsApp.html">WhatsApp</a> </button>
+               
+            
+                
+        </div>
+        <div class="class-menu">
+            
+            <button class="class-button"><a href="Instagram.html">Instagram</a> </button>
+           
+        
+            
+    </div>
+    <div class="class-menu">
+            
+  
+       
+        <button class="class-button"><a href="Partners.html">Partners</a> </button>
+        
+</div>
+<div class="class-menu">
+            
+    <button class="class-button"><a href="Pricing.html">Pricing</a> </button>
+   
+
+    
+</div>
+<div class="class-menu">
+            
+<button class="class-button"><a href="Login.html">Login</a> </button>
+
+
+
+</div>
+<div class="class-menu">
+            
+<button id="Free_trail"><a href="FreeTrial.html">Free Trial</a> </button>
+
+
+</div>
+<div class="class-menu">
+            
+<button id="book_demo"><a href="BookDemo.html">Book Demo</a> </button>
+
+
+</div>
+    </div>
+    
+</div>
+
+</div>  `
+}
+export default navbars;
