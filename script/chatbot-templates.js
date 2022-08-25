4eedddf44fb83199eb72eb0d7e5d2c3e0e1548cf
mahipat303 <memahipat303@gmail.com>
2022-08-26 00:58:08 +0530

    chatBot template js file added

---
+++ script/chatbot-templates.js
@@ -0,0 +1,288 @@
+var templates = [
+  {
+    title: "SURVEY BOT",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/survey_bot.jpg",
+    des: "Collect feedback intelligently from your website guests with a survey chatbot. It helps in getting live website feedback & in-app product feedback.",
+    cat:""
+  },
+  {
+    title: "Feedback Bot",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/feedback_bot.jpg",
+    des: "Easily get reviews for your product or services from your customers, teammates, peers, and managers. Convert feedback into actionable insights to enhance the quality.",
+  },
+  {
+    title: "APPOINTMENT BOOKING",
+    img: "https://custpostimages.s3.ap-south-1.amazonaws.com/sb_images/industry_images/appointment_booking.jpg",
+    des: "Book meetings & improve customer relationships, grab and nurture quality leads and increase your reach 4X with Multilingual, omnichannel conversational bots.",
+  },
+  {
+    title: "PLAYSCHOOL",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/playschool.jpg",
+    des: "Chatbot to get complete details of play schools that parents are looking to join their child and help them schedule a quick call or appointment with teachers.",
+  },
+  {
+    title: "HOME RENTAL",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/home_rental.jpg",
+    des: "A chatbot can present personalized offers to users keen on renting/purchasing a property and collects their details. It helps salespeople to know more about the prospect's intent.",
+  },
+  {
+    title: "CAR HIRE",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/car_hire.jpg",
+    des: "Car rental chatbots enable customers to buy or rent cars through a bot that is available 24/7. Get complete details of the car and pricing on a single platform. ",
+  },
+  {
+    title: "VISA CONSULTANCY",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/visa_conultancy.jpg",
+    des: "Chatbot that helps you delegate the redundant task of initial student assessment. Find the card details and entire process by chatting with a live agent on the chatbot.",
+  },
+  {
+    title: "TRAVEL AGENCY",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/travel_package_booking.jpg",
+    des: "A chatbot can become a guide to your customer while traveling. Helps customers in bookings, travel planning, tour promotions, and others on fingertips.",
+  },
+  {
+    title: "FLAT FOR SALE",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/flat_for_sale.jpg",
+    des: "A chatbot can help you get the contact details and interests of your prospects and help your prospects provide complete information on the type of property they are keen on. ",
+  },
+  {
+    title: "HOME RENTAL",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/home_rental_other.jpg",
+    des: "Deploy chatbots in real-estate businesses to help customers get options available to them based on their interest and complete details of renting a home.",
+  },
+  {
+    title: "HR CONSULTANCY",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/hr_consultancy.jpg",
+    des: "Chatbot helps HR to interact with employees and assist them with their routine queries and tasks. Manage, and retain employees across the globe with 24/7 available multilingual support.",
+  },
+  {
+    title: "E-COMMERCE SHOPPING",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/ecommerce_shopping.jpg",
+    des: "Easily get reviews for your product or services from your customers, teammates, peers, and managers. Convert feedback into actionable insights to enhance the quality.",
+  },
+  {
+    title: "SMATBOT LOANS",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/smatbot_loans.jpg",
+    des: "Easily get reviews for your product or services from your customers, teammates, peers, and managers. Convert feedback into actionable insights to enhance the quality.",
+  },
+  {
+    title: "START-UP FUNDING",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/start_up_funding.jpg",
+    des: "Chatbot helps HR to interact with employees and assist them with their routine queries and tasks. Manage, and retain employees across the globe with 24/7 available multilingual support.",
+  },
+  {
+    title: "DIET CONSULTANT",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/diet_consultant.jpg",
+    des: "Easily get reviews for your product or services from your customers, teammates, peers, and managers. Convert feedback into actionable insights to enhance the quality.",
+  },
+  {
+    title: "GYM ENQUIRY",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/gym_enquiry.jpg",
+    des: "Easily get reviews for your product or services from your customers, teammates, peers, and managers. Convert feedback into actionable insights to enhance the quality.",
+  },
+  {
+    title: "DIGITAL MARKETING AGENCY",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/digital_agency.jpg",
+    des: "Chatbot helps HR to interact with employees and assist them with their routine queries and tasks. Manage, and retain employees across the globe with 24/7 available multilingual support.",
+  },
+  {
+    title: "HOTEL BOOKING",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/hotel_booking.jpg",
+    des: "Easily get reviews for your product or services from your customers, teammates, peers, and managers. Convert feedback into actionable insights to enhance the quality.",
+  },
+];
+var templates2 = [
+  {
+    title: "AI VOICE DEMO",
+    img: "https://custpostimages.s3.ap-south-1.amazonaws.com/sb_images/industry_images/voice_demo.jpg",
+    des: "Just like you do in Whatsapp and other messaging apps. You can press the Speaker button and speak to give voice input.",
+  },
+  {
+    title: "UNIVERSITY ENROLLMENT",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/university_enrollment.jpg",
+    des: "Streamline support, improve admissions, automate enrolling processes without intervention of human agents using conversational AI bots.",
+  },
+  {
+    title: "TRAVEL AGENCY SUPPORT",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/trvale_agency_support.jpg",
+    des: "Support your customers while traveling and give instant responses for inquiries, free city guides, bookings, tour offers, travel planning, and suggestions using conversational AI bots.",
+  },
+  {
+    title: "E-COMMERCE SUPPORT",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/ecommerce_support.jpg",
+    des: "Provide digital assistance to your customer at their entire customer lifecycle by solving their queries at no time using conversational AI bots.",
+  },
+  {
+    title: "BANKING SERVICES",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/banking_services.jpg",
+    des: "Assist your customers in reviewing an account, reporting a loss of card, making payments, renewing any policy, and all other banking support at their fingertips.",
+  },
+  {
+    title: "SMATBOT RESTAURANT",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/smatbot_restaurant.jpg",
+    des: "Help customers book a table, order food online, solve customer queries, promote deals and offers, recommend food and assist customers in billing using conversational AI bots.",
+  },
+  {
+    title: "CINEMA HELP DESK",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/cinema_help_desk.jpg",
+    des: "Help your customers know trending movies, ticket availability, booking tickets, and solving refunds and other issues using automated conversational AI bots.",
+  },
+  {
+    title: "SOFTWARE PROVIDER",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/software_provider.jpg",
+    des: "Provide details of the software, features, compatibility, pricing, and other information automatically to your customers using conversational AI bots.",
+  },
+  {
+    title: "PARCEL COMPANY",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/parcel_company.jpg",
+    des: "Help your customers track the parcel's status, parcel charges, limitations to areas, and further details using conversational AI bots.",
+  },
+  {
+    title: "ISP HELP DESK",
+    img: "https://s3.ap-south-1.amazonaws.com/custpostimages/sb_images/industry_images/isp_help_desk.jpg",
+    des: "Help your customers raise tickets for no/weak internet, payment-related queries, and other issues using automated conversational AI bots.",
+  },
+  {
+    title: "HR INTERNAL HELP DESK",
+    img: "https://custpostimages.s3.ap-south-1.amazonaws.com/sb_images/industry_images/hr_internal_help_desk.jpeg",
+    des: "This chat BOT help you raise a ticket to the HR team and connects you to the right stakeholder.",
+  },
+];
+
+list1 = [
+  {
+    li: "All",
+  },
+  {
+    li: "Usecases",
+  },
+  {
+    li: "Education",
+  },
+  {
+    li: "Medical",
+  },
+  {
+    li: "Rental",
+  },
+  {
+    li: "Travel",
+  },
+  {
+    li: "Finance",
+  },
+  {
+    li: "Real Estate",
+  },
+  {
+    li: "Fitness",
+  },
+  {
+    li: "Agency",
+  },
+  {
+    li: "Software",
+  },
+  {
+    li: "HR",
+  },
+  {
+    li: "E-Commerce",
+  },
+  {
+    li: "Event Organizing",
+  },
+  {
+    li: "Franchise",
+  },
+  {
+    li: "Hotels & Restaurants",
+  },
+  {
+    li: "Miscellaneous",
+  },
+];
+
+list2 = [
+  {
+    li: "All",
+  },
+  {
+    li: "Education",
+  },
+  {
+    li: "Travel",
+  },
+  {
+    li: "Finance",
+  },
+  {
+    li: "Software",
+  },
+  {
+    li: "E-Commerce",
+  },
+  {
+    li: "Hotels & Restaurants",
+  },
+  {
+    li: "Entertainment",
+  },
+  {
+    li: "Tech Support",
+  },
+  {
+    li: "Miscellaneous",
+  },
+];
+
+var category_selector = document.getElementById("category_selector");
+category_selector.addEventListener("change", function () {
+  let value = category_selector.value;
+  if (value == "L_G_B") {
+    append(templates);
+    ulAppend(list1);
+  } else {
+    append(templates2);
+    ulAppend(list2);
+  }
+});
+
+var container = document.getElementById("template_container");
+
+let append = (data) => {
+  container.innerHTML = null;
+  data.forEach((ele) => {
+    let div = document.createElement("div");
+    div.className = "temp_box";
+    let title = document.createElement("h3");
+    let img = document.createElement("img");
+    let des = document.createElement("label");
+    let btn = document.createElement("button");
+    btn.innerText = "Use This";
+    btn.id = "select_temp_btn";
+    title.innerText = ele.title;
+    img.src = ele.img;
+    des.innerText = ele.des;
+    div.append(title, img, des, btn);
+    container.append(div);
+  });
+};
+append(templates);
+var ul = document.getElementById("ul");
+var ulAppend = (data) => {
+  ul.innerHTML = null;
+  data.forEach((ele) => {
+    let li = document.createElement("li");
+    li.innerText = ele.li;
+    li.addEventListener("click", function () {
+      temFilter(li.innerText);
+    });
+    ul.append(li);
+  });
+};
+ulAppend(list1);
+
+function temFilter(list) {
+  console.log(list);
+}
